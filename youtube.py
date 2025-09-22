ce3d17dd743427dafd104a7e12c1c45815bbf36a
Yezdani01 <yezdanihamza@gmail.com>
2025-09-22 23:48:35 +0530

    few changes

--- youtube.py
+++ youtube.py
@@ -1,27 +1,38 @@
-from pytube import Youtube
+from pytube import YouTube
 import tkinter as tk
 from tkinter import filedialog  
 
 
-def download_video(url, save_path):
-    try:
-        yt = Youtube(url)
-        streams = yt.streams.filter(progressive = True, file_extension = "mp4")
-        highest_res_stream = streams.get_highest_resolution()
-        highest_res_stream.download(output_path = save_path)
-        print("Video downloaded Sucessfully !!!")      
-    except Exception as e:
-        print(e)
+# def download_video(url, save_path):
+#     try:
+#         yt = YouTube(url)
+#         streams = yt.streams.filter(progressive = True, file_extension = "mp4")
+#         highest_res_stream = streams.get_highest_resolution()
+#         highest_res_stream.download(output_path = save_path)
+#         print("Video downloaded Sucessfully !!!")      
+#     except Exception as e:
+#         print(e)
 
 
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"Selected Folder : {folder}")
+    return folder
 
 
+if __name__ == "__main__":
+    root = tk.Tk() 
+    root.withdraw()#hide the window
+
+    video_url = input("Please Enter a Youtube URL: ")
+    save_dir = open_file_dialog()
+
+    if save_dir:
+        print("--------_Started Download _---------")
+        download_video(video_url,save_dir)
+
+    else:
+        print("Invalid Save Location")
 
 
-
-
-        
-    url = ""
-    save_path = ""
-
-    download_video(url, save_path)
