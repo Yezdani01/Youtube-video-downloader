32b387559165a6833b1842f8d1f6fbdef8babe78
Yezdani01 <yezdanihamza@gmail.com>
2025-09-25 12:37:24 +0530

    few changes

--- youtube.py
+++ youtube.py
@@ -3,15 +3,15 @@ import tkinter as tk
 from tkinter import filedialog  
 
 
-# def download_video(url, save_path):
-#     try:
-#         yt = YouTube(url)
-#         streams = yt.streams.filter(progressive = True, file_extension = "mp4")
-#         highest_res_stream = streams.get_highest_resolution()
-#         highest_res_stream.download(output_path = save_path)
-#         print("Video downloaded Sucessfully !!!")      
-#     except Exception as e:
-#         print(e)
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)
+        streams = yt.streams.filter(progressive = True, file_extension = "mp4")
+        highest_res_stream = streams.get_highest_resolution()
+        highest_res_stream.download(output_path = save_path)
+        print("Video downloaded Sucessfully !!!")      
+    except Exception as e:
+        print(e)
 
 
 def open_file_dialog():
